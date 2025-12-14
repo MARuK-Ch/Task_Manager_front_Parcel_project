fe7f1d8b0ac91085055bc5be5d2e58edd704244c
MARuK-Ch <marikhasa1983@gmail.ru>
2025-12-14 22:10:17 +0500

    some commit

--- src/repositories/MongoDBTaskRepository.ts
+++ src/repositories/MongoDBTaskRepository.ts
@@ -2,43 +2,45 @@ import {MongoDBTaskRepositoryInterface, UpdateTaskPayLoad} from "./TaskRepositor
 import {Task} from "../models/TaskModel";
 import axios from "axios";
 
+const BASE_URL = `${process.env.API_URL}/api/tasks` || 'http://localhost:5000/api/tasks'
+
 export default class MongoDbTaskRepository implements MongoDBTaskRepositoryInterface {
     constructor() {}
 
 
     async getAll(): Promise<Task[]> {
-        const response = await axios.get('http://localhost:5000/api/tasks')
+        const response = await axios.get(BASE_URL)
         return response.data
     }
 
     async getCurrentTasks(): Promise<Task[]> {
-        const response = await axios.get('http://localhost:5000/api/tasks/pending')
+        const response = await axios.get(`${BASE_URL}/pending`)
         return response.data
     }
 
     async getDoneTasks(): Promise<Task[]> {
-        const response = await axios.get('http://localhost:5000/api/tasks/done')
+        const response = await axios.get(`${BASE_URL}/done`)
         return response.data
     }
 
     async add(title: string) {
-        await axios.post(`http://localhost:5000/api/tasks/create`, {
+        await axios.post(`${BASE_URL}/create`, {
             title: title
         })
     }
 
     async remove(id: string) {
-        await axios.delete(`http://localhost:5000/api/tasks/${id}`)
+        await axios.delete(`${BASE_URL}/${id}`)
     }
 
     async updateTitle(id: string, newTitle: string) {
-        await axios.patch(`http://localhost:5000/api/tasks/${id}/title`, {
+        await axios.patch(`${BASE_URL}/${id}/title`, {
             newTitle: newTitle
         })
     }
 
     async updateStatus(id: string, newStatus: boolean) {
-        await axios.patch(`http://localhost:5000/api/tasks/${id}/status`, {
+        await axios.patch(`${BASE_URL}/${id}/status`, {
             newStatus: newStatus
         })
     }
